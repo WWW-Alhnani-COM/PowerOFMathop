8ff08a1d66b57d5d97b057495a81766744d48a0e
Mohammad Alhnani <145880506+WWW-Alhnani-COM@users.noreply.github.com>
2026-04-17 05:13:50 +0300

    Refactor supabaseServer to use createServerClient
    Refactor supabaseServer function to use createServerClient and handle cookies.

--- actions/chat.actions.js
+++ actions/chat.actions.js
@@ -1,16 +1,28 @@
 'use server'
 
-import { createClient } from '@supabase/supabase-js'
+import { createServerClient } from "@supabase/ssr";
 import { cookies } from 'next/headers'
-
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 // =====================================================
 // Helper: Supabase Server Client
 // =====================================================
-function supabaseServer() {
-  return createClient(
-    process.env.SUPABASE_SERVICE_ROLE_KEYL,
-    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY
-  )
+async function supabaseServer() {
+  const cookieStore = await cookies();
+
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY,
+    {
+      cookies: {
+        getAll() {
+          return cookieStore.getAll();
+        },
+        setAll() {}
+      }
+    }
+  );
 }
 
 // =====================================================
